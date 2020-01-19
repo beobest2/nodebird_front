1392eb7effa2a6ab29ac7dea13dfa7c8bb05a74f
hyunwoo <cleanby@naver.com>
2020-01-20 01:18:24 +0900

    6-wow

--- components/PostCard.js
+++ components/PostCard.js
@@ -3,7 +3,7 @@ import { Button, Icon, Card, Avatar, Form, Input, List, Comment } from 'antd';
 import Link from 'next/Link';
 import PropTypes from 'prop-types';
 import { useSelector, useDispatch } from 'react-redux';
-import { ADD_COMMENT_REQUEST, LOAD_COMMENTS_REQUEST } from '../reducers/post';
+import { ADD_COMMENT_REQUEST, LOAD_COMMENTS_REQUEST, UNLIKE_POST_REQUEST, LIKE_POST_REQUEST, RETWEET_REQUEST } from '../reducers/post';
 import PostImages from './PostImages';
 
 const PostCard = ({ post }) => {
@@ -12,6 +12,9 @@ const PostCard = ({ post }) => {
     const { me } = useSelector(state => state.user);
     const { isAddingComment, commentAdded } = useSelector(state => state.post);
     const dispatch = useDispatch();
+
+    const liked = me && post.Likers && post.Likers.find(v => v.id === me.id);
+    //console.log(post.Likers);
 
     const onToggleComment = useCallback(() => {
         setCommentFormOpened(prev => !prev);
@@ -45,14 +48,43 @@ const PostCard = ({ post }) => {
         setCommentText(e.target.value);
     }, []);
 
+    const onToggleLike = useCallback(() => {
+        if(!me) {
+            return alert('you need to login');
+        }
+        if (liked) {
+            // already liked
+            dispatch({
+                type: UNLIKE_POST_REQUEST,
+                data: post.id,
+            });
+        } else {
+            // yet liked
+            dispatch({
+                type: LIKE_POST_REQUEST,
+                data: post.id,
+            });
+        }
+    }, [me && me.id, post && post.id, liked]);
+
+    const onRetweet = useCallback(() => {
+        if (!me) {
+            return alert('you neet to login');
+        }
+        return dispatch({
+            type: RETWEET_REQUEST,
+            data: post.id,
+        })
+    }, [me && me.id, post && post.id]);
+
     return (
         <div>
             <Card
                 key = {+post.createdAt}
                 cover = {post.Images[0] && <PostImages images={post.Images} />}
                 actions = {[
-                    <Icon type="retweet" key="retweet" />,
-                    <Icon type="heart" key="heart" />,
+                    <Icon type="retweet" key="retweet" onClick={onRetweet}/>,
+                    <Icon type="heart" key="heart" theme={liked ? 'twoTone' : 'outlined'} twoToneColor='#eb2f96' onClick={onToggleLike}/>,
                     <Icon type="message" key="message" onClick={onToggleComment} />,
                     <Icon type="ellipsis" key="ellipsis" />,
                 ]}
